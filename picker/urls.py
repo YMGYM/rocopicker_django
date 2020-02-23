88d5f7a3f42dd33aa9412d6067623e3e5b651698
YMGYM <good_day1234@naver.com>
2020-02-23 04:59:04 +0000

    write basic pages

--- picker/urls.py
+++ picker/urls.py
@@ -3,6 +3,9 @@ from django.urls import path
 
 # Create your views here.
 urlpatterns = [
-    path('', views.mainpage, name="mainpage"),
+    path('', views.mainpage, name="mainPage"),
+    path('inputPage/', views.inputPage, name="inputPage"),
+    path('result/', views.resultPage, name="resultPage"),
+    path('credit/', views.creditPage, name="creditPage"),
 ]
 
--- picker/views.py
+++ picker/views.py
@@ -3,3 +3,18 @@ from django.shortcuts import render
 def mainpage(request):
     
     return render(request, 'picker/mainpage.html')
+
+
+def inputPage(request):
+    
+    return render(request, 'picker/inputpage.html')
+
+
+def resultPage(request):
+    
+    return render(request, 'picker/resultPage.html')
+
+
+def creditPage(request):
+    
+    return render(request, 'picker/creditPage.html')
